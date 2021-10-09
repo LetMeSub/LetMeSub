66ddeab4fea8edfc503b12dc7fcc72ee689a7fdf
nbgh46 <nbgh46@naver.com>
2021-10-09 14:25:39 +0900

    dto 설정

---
+++ src/main/java/com/example/letmesub/dto/subscribe.java
@@ -0,0 +1,19 @@
+package com.example.letmesub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class subscribe
+{
+
+    String name;
+    String describe;
+    String category;
+    String imgPath;
+    int weight;
+    int count;
+
+
+}
