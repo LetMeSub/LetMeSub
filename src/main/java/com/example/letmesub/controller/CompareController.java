740d7b70c2c3419f9d0c55ac5b277b00f6c1859e
choyoungwoo9 <choyoungwoo9@naver.com>
2021-10-11 13:20:32 +0900

    비교 페이지 ui 추가

--- src/main/java/com/example/letmesub/controller/CompareController.java
+++ src/main/java/com/example/letmesub/controller/CompareController.java
@@ -1,9 +1,14 @@
 package com.example.letmesub.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class CompareController
 {
+    @GetMapping("/compare")
+    public String compare(){
 
+        return "compare";
+    }
 }
