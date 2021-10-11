7113c4a1a714d8d64bcff01923d1e6bb123ec37f
choyoungwoo9 <choyoungwoo9@naver.com>
2021-10-11 15:58:29 +0900

    유저선호도 arraylist로 받아옴

--- src/main/java/com/example/letmesub/controller/RecommendController.java
+++ src/main/java/com/example/letmesub/controller/RecommendController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.letmesub.recommend.recAlgo;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 // 조영우
 
 @Controller
@@ -17,13 +19,30 @@ public class RecommendController
     @GetMapping("/recommend")
     public String recommend(Model model, HttpServletRequest req)
     {
+        //첫 접속시 count값 할당
         if(req.getQueryString() == null){
             int qcount = 1;
-            model.addAttribute("count", qcount);
+            model.addAttribute("count",qcount);
             return "recommend";
         }
+        
+        //html에서 값 받아옴
         int qcount = Integer.parseInt(req.getParameter("count"));
+        int uweight = Integer.parseInt(req.getParameter("weight"));
+
+        //weight 세션 처리
+        HttpSession session = req.getSession();
+        ArrayList<Integer> list = (ArrayList)session.getAttribute("weight");
+        if(list==null){
+            list = new ArrayList<Integer>();
+            session.setAttribute("weight",list);
+        }
+        list.add(uweight);
+        System.out.println(list);
+        
+        //모델에 count 전달
         model.addAttribute("count", qcount);
+
         if(qcount == 7){
             return "recommend_selected";
         }
