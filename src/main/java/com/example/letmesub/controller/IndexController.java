4c438444235dd7cd628abab3342cd83d55f350f4
nbgh46 <nbgh46@naver.com>
2021-10-23 18:24:38 +0900

    이미지 추가, 인덱스 작업 시작

--- src/main/java/com/example/letmesub/controller/IndexController.java
+++ src/main/java/com/example/letmesub/controller/IndexController.java
@@ -1,14 +1,18 @@
 package com.example.letmesub.controller;
 // 유민상
 
+import com.example.letmesub.dao.IndexDao;
+import com.example.letmesub.dao.UserDao;
+import com.example.letmesub.dto.SubscribeDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -41,5 +45,29 @@ public class IndexController
         return result;
 
     }
+
+    @Autowired
+    private IndexDao indexDao;
+
+    // 인덱스에서 카테고리 선택시 해당 카테고리에 해당하는 데이터를 가져와 뿌려주는 함수
+    @GetMapping("/api/index")
+    public Map<String, String> SetIndexContent(String category)
+    {
+
+        // DB 에서 해당 카테고리 가져오기
+        List<SubscribeDto> SelectedList = indexDao.IndexList(category);
+
+        for (SubscribeDto i : SelectedList)
+        {
+            System.out.println(i.getSubscribe_name());
+        }
+
+        // 반환값
+        Map<String, String> result = new HashMap<>();
+        result.put("result", "success");
+        return result;
+    }
+
+
 
 }
---
+++ src/main/java/com/example/letmesub/dao/IndexDao.java
@@ -0,0 +1,12 @@
+package com.example.letmesub.dao;
+
+import com.example.letmesub.dto.SubscribeDto;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IndexDao
+{
+    List<SubscribeDto> IndexList(String category);
+}
