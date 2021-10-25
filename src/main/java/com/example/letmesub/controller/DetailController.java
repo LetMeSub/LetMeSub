37cc6eadeaf9d16fa4a93acff1d16ea7f5a6f253
choyoungwoo9 <choyoungwoo9@naver.com>
2021-10-25 18:46:12 +0900

    detail페이지 별점기능, 상세정보 구현, 댓글기능 구현필

--- src/main/java/com/example/letmesub/controller/DetailController.java
+++ src/main/java/com/example/letmesub/controller/DetailController.java
@@ -1,5 +1,6 @@
 package com.example.letmesub.controller;
 
+import com.example.letmesub.dao.ICommentDao;
 import com.example.letmesub.dao.ISubscribeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 // 안주현
 
 @Controller
@@ -17,14 +19,22 @@ public class DetailController
     @Autowired
     private ISubscribeDao subscribeDao;
 
+    @Autowired
+    private ICommentDao commentdao;
+
     @GetMapping("/detail")
     public String detail(Model model, HttpServletRequest req)
     {
 
-        String subscribe = "netflix";
+        String subscribe = req.getParameter("subscribe_name");
+        System.out.println(subscribe);
         int rate = subscribeDao.viewAll(subscribe).getSubscribe_rate();
         String desc = subscribeDao.viewAll(subscribe).getSubscribe_describe();
 
+
+        System.out.println(commentdao.commentList(subscribe));
+
+        model.addAttribute("list", commentdao.commentList(subscribe));
         model.addAttribute("subscribe", subscribe);
         model.addAttribute("rate", rate);
         model.addAttribute("desc", desc);
